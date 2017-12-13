155ee9eb6913a2d4ca7abd398f230034852d866f
Michael Green <mcg520@nyu.edu>
2017-12-13 00:31:16 -0500

    Update for the CellularAutomata Structure Generator example code

--- stock-filters/CACS_Example.py
+++ stock-filters/CACS_Example.py
@@ -11,8 +11,8 @@ from mcplatform import *
 #inputs are taken from the user. Here I've just showing labels, as well as letting the user define
 # what the main creation material for the structures is
 inputs = (
-	("4Structures", "label"),
-	("Material:", alphaMaterials.Cobblestone), # the material we want to use to build the mass of the structures
+	("Cellular Automata SG Example", "label"),
+	("Material", alphaMaterials.Cobblestone), # the material we want to use to build the mass of the structures
 	("Creator: Michael Green", "label"),
 	)
 
@@ -70,40 +70,91 @@ def drawLineConstrained(scratchpad, (blockID, blockData), (x,y,z), (x1,y1,z1), m
 # Every agent must have a "perform" function, which has three parameters
 # 1: the level (aka the minecraft world). 2: the box
 def perform(level, box, options):
-	quadrants = splitIntoQuadrants(box)
+	quadrants = binaryPartition(box)
 	# for each quadrant
 	for quad in quadrants:
 		buildFence(level, quad)
-		buildStructure(level, quad)
+		buildStructure(level, quad, options)
 
 #splits the given box into 4 unequal areas
-def splitIntoQuadrants(box):
-	(width, height, depth) = getBoxSize(box)
-	centreWidth = width / 2
-	centreDepth = depth / 2
+def binaryPartition(box):
+	partitions = []
+	# create a queue which holds the next areas to be partitioned
+	queue = []
+	queue.append(box)
+	# for as long as the queue still has boxes to partition...
+	count = 0
+	while len(queue) > 0:
+		count += 1
+		splitMe = queue.pop(0)
+		(width, height, depth) = getBoxSize(splitMe)
+		# print "Current partition width,depth",width,depth 
+		centre = 0
+		# this bool lets me know which dimension I will be splitting on. It matters when we create the new outer bound size
+		isWidth = False
+		# find the larger dimension and divide in half
+		# if the larger dimension is < 10, then block this from being partitioned
+		minSize = 15
+		if width > depth:
+			if depth < minSize:
+				partitions.append(splitMe)
+				print "Current partition: xmin,zmin,xmax,zmax,width,depth",splitMe.minx, splitMe.minz, splitMe.maxx, splitMe.maxz, width,depth 
+				continue
+			isWidth = True
+			centre = width / 2
+		else:
+			if width < minSize:
+				partitions.append(splitMe)
+				print "Current partition: xmin,zmin,xmax,zmax,width,depth",splitMe.minx, splitMe.minz, splitMe.maxx, splitMe.maxz, width,depth 
+				continue				
+			centre = depth / 2
 
-	# a random modifier for quadrant splitting which is somewhere between 0 and 1/8 the total box width and depth
-	randomWidth = random.randint(-(centreWidth / 4), centreWidth / 4)
-	randomDepth = random.randint(-(centreDepth / 4), centreDepth / 4)
+		print "Centre", centre
+		# centreWidth = width / 2
+		# centreDepth = depth / 2
 
-	# creating the new bounds
-	newWidthBound = centreWidth + randomWidth
-	newDepthBound = centreDepth + randomDepth
+		# a random modifier for binary splitting which is somewhere between 0 and 1/8 the total box side length
+		randomPartition = random.randint(0, (centre / 4))
+		print "randomPartition", randomPartition
+		# randomWidth = random.randint(-(centreWidth / 4), centreWidth / 4)
+		# randomDepth = random.randint(-(centreDepth / 4), centreDepth / 4)
 
-	#creating the outer edge bounds
-	outsideWidthSize = width - newWidthBound - 1
-	outsideDepthSize = depth - newDepthBound - 1
+		# creating the new bound
+		newBound = centre + randomPartition
+		# newWidthBound = centreWidth + randomWidth
+		# newDepthBound = centreDepth + randomDepth
 
-	# creating the bounding boxes
-	# NOTE: BoundingBoxes are objects contained within pymclevel and can be instantiated as follows
-	# BoundingBox((x,y,z), (sizex, sizey, sizez))
-	# in this instance, you specifiy which corner to start, and then the size of the box dimensions
-	one = BoundingBox((box.minx, box.miny, box.minz), (newWidthBound-1, box.maxy, newDepthBound-1))
-	two = BoundingBox((box.minx+newWidthBound+1, box.miny, box.minz), (outsideWidthSize-1, box.maxy, newDepthBound-1))
-	three = BoundingBox((box.minx, box.miny, box.minz+newDepthBound+1), (newWidthBound-1, box.maxy, outsideDepthSize-1))
-	four = BoundingBox((box.minx+newWidthBound+1, box.miny, box.minz+newDepthBound+1), (outsideWidthSize-1, box.maxy, outsideDepthSize-1))
-	boxList = [one, two, three, four]
-	return boxList
+		#creating the outer edge bounds
+		outsideNewBounds = 0
+		if isWidth:
+			outsideNewBound = width - newBound - 1
+		else:
+			outsideNewBound = depth - newBound - 1
+		# outsideWidthSize = width - newWidthBound - 1
+		# outsideDepthSize = depth - newDepthBound - 1
+
+		# creating the bounding boxes
+		# NOTE: BoundingBoxes are objects contained within pymclevel and can be instantiated as follows
+		# BoundingBox((x,y,z), (sizex, sizey, sizez))
+		# in this instance, you specifiy which corner to start, and then the size of the box dimensions
+		if isWidth:
+			print "width change"
+			print "first", splitMe.minx, splitMe.miny, splitMe.minz, ":", newBound-1, 256, depth
+			print "second", splitMe.minx + newBound + 1, splitMe.miny, splitMe.minz, ":", outsideNewBound - 1, 256, depth
+			queue.append(BoundingBox((splitMe.minx, splitMe.miny, splitMe.minz), (newBound-1, 256, depth)))
+			queue.append(BoundingBox((splitMe.minx + newBound + 1, splitMe.miny, splitMe.minz), (outsideNewBound - 1, 256, depth)))
+		else:
+			print "depth change"
+			print "first",splitMe.minx, splitMe.miny, splitMe.minz, ":", width, 256, newBound - 1
+			print "second",splitMe.minx, splitMe.miny, splitMe.minz + newBound + 1, ":", width, 256, outsideNewBound - 1
+			queue.append(BoundingBox((splitMe.minx, splitMe.miny, splitMe.minz), (width, 256, newBound - 1)))
+			queue.append(BoundingBox((splitMe.minx, splitMe.miny, splitMe.minz + newBound + 1), (width, 256, outsideNewBound - 1)))
+		# one = BoundingBox((box.minx, box.miny, box.minz), (newWidthBound-1, box.maxy, newDepthBound-1))
+		# two = BoundingBox((box.minx+newWidthBound+1, box.miny, box.minz), (outsideWidthSize-1, box.maxy, newDepthBound-1))
+		# three = BoundingBox((box.minx, box.miny, box.minz+newDepthBound+1), (newWidthBound-1, box.maxy, outsideDepthSize-1))
+		# four = BoundingBox((box.minx+newWidthBound+1, box.miny, box.minz+newDepthBound+1), (outsideWidthSize-1, box.maxy, outsideDepthSize-1))
+		# boxList = [one, two, three, four]
+	return partitions
 
 # builds a wooden fence around the perimeter of this box, like this photo
 #			  Top - zmax
@@ -165,35 +216,37 @@ def buildFence(level, box):
 # 2. create corner pillars
 # 3. between each pair of pillars, use Cellular Automata to build a wall
 # 4. create celing at the ceiling level
-def buildStructure(level, box):
+def buildStructure(level, box, options):
 	floor = makeFloorPlan(level, box)
-	buildingHeightInfo = createPillars(level, floor)
-	generateWalls(level, floor, buildingHeightInfo)
-	generateCeiling(level, floor, buildingHeightInfo)
+	buildingHeightInfo = createPillars(level, floor, options)
+	generateWalls(level, floor, buildingHeightInfo, options)
+	generateCeiling(level, floor, buildingHeightInfo, options)
 
 def makeFloorPlan(level, box):
 	# we have to first figure out where in the box this is going to be
 	# find the box dimensions
 	(width, height, depth) = getBoxSize(box)
 
-	# get eights
-	fractionWidth = width / 4
-	fractionDepth = depth / 4
-	print (fractionWidth)
-	print (fractionDepth)
+	# get sixths
+	fractionWidth = width / 6
+	fractionDepth = depth / 6
+	# print (fractionWidth)
+	# print (fractionDepth)
 	# create the box boundaries
-	xstart = box.minx + random.randint(0, fractionWidth) + 1
-	zstart = box.minz + random.randint(0, fractionDepth) + 1
+	randFracx = random.randint(0, fractionWidth+1)
+	randFracz = random.randint(0, fractionDepth+1)
+	xstart = box.minx +  randFracx + 2
+	zstart = box.minz + randFracz + 2
 
-	xsize = width - random.randint(0, fractionWidth) - 5
-	zsize = depth - random.randint(0, fractionDepth) - 5
+	xsize = width * 0.6 - randFracx
+	zsize = depth * 0.6 - randFracz
 
 	floorplan = BoundingBox((xstart, box.miny, zstart), (xsize, box.maxy, zsize))
 	return floorplan
 
 # we need to create the corners for the walls.
 #Every building needs corners for stability...unless you are inventive... :)
-def createPillars(level, floor):
+def createPillars(level, floor, options):
 	cornerBlockStarts = []
 	ycoords = []
 	# similarly to fences, we need to countdown on each of the four corners and find the block where the ground starts, then start building pillars above that height
@@ -231,7 +284,7 @@ def createPillars(level, floor):
 			ystartCoordMax = cornerstone[1]
 		pillarheight = random.randint(5, 10)
 		for y in range(0, pillarheight):
-			setBlock(level, (4,0), cornerstone[0], cornerstone[1]+y, cornerstone[2])
+			setBlock(level, (options["Material"].ID,0), cornerstone[0], cornerstone[1]+y, cornerstone[2])
 			if(y==pillarheight-1):
 				# add y to our y coords, which will be used to determine building height for the roof
 				ycoords.append(y)
@@ -240,7 +293,7 @@ def createPillars(level, floor):
 		allYs += ycoord
 	yavg = allYs / 4
 	midpointFloorHeight = midpointFloorHeight / 4
-	print("Average pillar height: ", yavg)
+	# print("Average pillar height: ", yavg)
 	return (yavg, ystartCoordMax, midpointFloorHeight)
 
 # the walls of the building are generated each using independent ceullular automata. We look at the immediate neighborhood and take action
@@ -248,7 +301,7 @@ def createPillars(level, floor):
 # 1. intitialize with random block placement in the space
 # 2. evaluate each cell, checking its neighbors to gauge changes
 # 3. repeat 2 until satisfied
-def generateWalls(level, floor, buildingHeightInfo):
+def generateWalls(level, floor, buildingHeightInfo, options):
 	print "Generating walls"
 	# actual automata is going to be simulated in a matrix (it's much faster than rendering it in minecraft)
 	# first we should define the matrix properties (i.e. width and height)
@@ -256,84 +309,84 @@ def generateWalls(level, floor, buildingHeightInfo):
 	height = buildingHeightInfo[0]
 	print "X walls"
 	for k in range(2):
-		print "i:", k
-		print("matrix width: ", width, "Matrix height: ", height)
+		# print "i:", k
+		# print("matrix width: ", width, "Matrix height: ", height)
 		# we have our matrix for CA, now lets do CA
 		matrix = [[0 for x in range(width)] for y in range(height)]
 		matrixnew = randomlyAssign(matrix, width, height)
-		for j in range(height):
-			for i in range(width):
-				print matrixnew[j][i],
-			print
+		# for j in range(height):
+		# 	for i in range(width):
+		# 		print matrixnew[j][i],
+		# 	print
 		# do 3 generations
 		for gen in range(0,2):
-			print "Generation ", gen
+			# print "Generation ", gen
 			matrixnew = cellularAutomataGeneration(matrixnew, width, height)
-			for j in range(height):
-				for i in range(width):
-					print matrixnew[j][i],
-				print
+			# for j in range(height):
+			# 	for i in range(width):
+			# 		print matrixnew[j][i],
+			# 	print
 		#after generation is over, place the walls according to the wall matrix, starting at the floor
 		for y in range(height):
 			for x in range(1,width):
 				if k==1:
-					print "boom 1"
+					# print "boom 1"
 					if matrixnew[y][x] == 1:
-						setBlock(level, (4, 0), floor.minx+x, buildingHeightInfo[2] + y, floor.minz)
+						setBlock(level, (options["Material"].ID, 0), floor.minx+x, buildingHeightInfo[2] + y, floor.minz)
 					else:
 						setBlock(level, (20, 0), floor.minx+x, buildingHeightInfo[2] + y, floor.minz)
 				else:
-					print "boom 2"
+					# print "boom 2"
 					if matrixnew[y][x] == 1:
-						setBlock(level, (4, 0), floor.minx+x, buildingHeightInfo[2] + y, floor.maxz)
+						setBlock(level, (options["Material"].ID, 0), floor.minx+x, buildingHeightInfo[2] + y, floor.maxz)
 					else:
 						setBlock(level, (20, 0), floor.minx+x, buildingHeightInfo[2] + y, floor.maxz)
 	print "Z Walls"
 	for k in range(2):
-		print "j:",j
+		# print "j:",j
 		# we have our matrix for CA, now lets do CA
 		matrix = [[0 for x in range(depth)] for y in range(height)]
 		matrixnew = randomlyAssign(matrix, depth, height)
-		print "pre cellular automata"
-		for j in range(height):
-			for i in range(depth):
-				print matrixnew[j][i],
-			print
+		# print "pre cellular automata"
+		# for j in range(height):
+		# 	for i in range(depth):
+		# 		print matrixnew[j][i],
+		# 	print
 		# do 3 generations
 		for gen in range(0,2):
 			print "Generation ", gen
 			matrixnew = cellularAutomataGeneration(matrixnew, depth, height)
-			for j in range(height):
-				for i in range(depth):
-					print matrixnew[j][i],
-				print
+			# for j in range(height):
+			# 	for i in range(depth):
+			# 		print matrixnew[j][i],
+			# 	print
 		#after generation is over, place the walls according to the wall matrix, starting at the floor
 		for y in range(height):
 			for z in range(1,depth):
 				if k==1:
-					print "boom 3"
+					# print "boom 3"
 					if matrixnew[y][z] == 1:
-						setBlock(level, (4, 0), floor.minx, buildingHeightInfo[2] + y, floor.minz+z)
+						setBlock(level, (options["Material"].ID, 0), floor.minx, buildingHeightInfo[2] + y, floor.minz+z)
 					else:
 						setBlock(level, (20, 0), floor.minx, buildingHeightInfo[2] + y, floor.minz+z)
 				else:
-					print "boom 4"
+					# print "boom 4"
 					if matrixnew[y][z] == 1:
-						setBlock(level, (4, 0), floor.maxx, buildingHeightInfo[2] + y, floor.minz+z)
+						setBlock(level, (options["Material"].ID, 0), floor.maxx, buildingHeightInfo[2] + y, floor.minz+z)
 					else:
 						setBlock(level, (20, 0), floor.maxx, buildingHeightInfo[2] + y, floor.minz+z)
 def randomlyAssign(matrix, width, height):
 	print 'randomly assigning to matrix'
 	for j in range(height):
 		for i in range(width):
-			print j,i
+			# print j,i
 			matrix[j][i] = random.randint(0,2)
 	return matrix
 
 def cellularAutomataGeneration(matrix, width, height):
 	for j in range(height):
 		for i in range(width):
-			print j,i
+			# print j,i
 			if j == 0 : #special case for bottom
 				matrix[j][i] = decideCell(1, matrix[j+1][i])
 			elif j == height-1 : #special case for top
@@ -369,8 +422,8 @@ def decideCell(top, bottom):
 # puts a cap on the building in question
 # uses the floor to determine the celing size, and the buildingHeightInfo tuple
 # to place it at the right level
-def generateCeiling(level, floor, buildingHeightInfo):
+def generateCeiling(level, floor, buildingHeightInfo, options):
 	print "generating ceiling"
 	for x in range(floor.minx, floor.maxx+1):
 		for z in range(floor.minz, floor.maxz+1):
-			setBlock(level, (4, 0), x, buildingHeightInfo[2] + buildingHeightInfo[0], z)
+			setBlock(level, (options["Material"].ID, 0), x, buildingHeightInfo[2] + buildingHeightInfo[0], z)
